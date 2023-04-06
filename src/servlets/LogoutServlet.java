f3ee270ca09903eb692d45062b746075b885f4c7
w2 <tanat443@gmail.com>
2023-04-06 12:53:25 +0600

    FinalProject final_commit

---
+++ src/servlets/LogoutServlet.java
@@ -0,0 +1,18 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().removeAttribute("currentUser");
+        response.sendRedirect("/login");
+    }
+}
